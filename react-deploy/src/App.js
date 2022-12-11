9ad674f4bd73183f2cfe2572d89d806c2c2afa07
inest-us <hieung@live.com>
2022-12-11 10:40:48 -0800

    check-in 2022-12-11 Part 2

--- react-deploy/src/App.js
+++ react-deploy/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import EventBind from './components/EventBind';
+// import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
     </div>
   );
 }
